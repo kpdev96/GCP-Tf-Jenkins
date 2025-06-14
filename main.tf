b7e8971bb06919f1adea01d9427cbae2299cb4af
Krishna Pavan <133009685+kpdev96@users.noreply.github.com>
2025-06-14 23:43:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tf-githubdemo-bucket-001"
-  project                  = healthy-booth-386907"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
+resource "google_storage_bucket" "storage-53" {
+  name     = "my-demo-6789"
+  project  = "healthy-booth-386907"
+  location = "asia-southeast1"
 }
-#
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tf-githubdemo-bucket-002"
-  project                  = "healthy-booth-386907"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
-#
-### dummy
